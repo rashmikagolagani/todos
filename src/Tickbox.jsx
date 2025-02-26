02f4843f88513695ea23da52a3a13b9fddb1a2b9
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-26 23:02:04 +0530

    added new css classes and included them in the tickbox and todoitems

--- src/Tickbox.jsx
+++ src/Tickbox.jsx
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 export default function Checkbox() {
   const [checkbox, setCheckbox] = useState("");
   return (
-    <div>
-      <input
-        type="checkbox"
-        checked={checkbox}
-        onChange={(e) => setCheckbox(e.target.checked)}
-        required
-      />
-    </div>
+    <input
+      type="checkbox"
+      checked={checkbox}
+      onChange={(e) => setCheckbox(e.target.checked)}
+      required
+    />
   );
 }
--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Checkbox from "./Tickbox";
 import { Trash2 } from "lucide-react";
+import { Pencil } from "lucide-react";
+
 //import dustbin from "../assets/dustbin.png";
 
 export default function TodoItems({ text }) {
   return (
     <div>
-      <div>
-        <Checkbox />
-        <p>{text}</p>
-        <div>
-          <Trash2 />
-        </div>
-      </div>
+      <p>
+        <Checkbox className="checkbox" /> {text} <Pencil className="edit" />{" "}
+        <Trash2 />
+      </p>
     </div>
   );
 }
