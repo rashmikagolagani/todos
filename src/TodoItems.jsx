412232fd754412095a41b4a7701a11ab908194bc
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-03-13 12:56:42 +0530

    added all the items in the TodoItems, it is a component with checkbox, clickEdit, EditTodo, DeleteTodo, DeleteConfirmation.

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Checkbox from "./Tickbox";
-import { Trash2 } from "lucide-react";
-import { Pencil } from "lucide-react";
+import EditTodo, { ClickEdit } from "./EditTodo";
+import DeleteConfirmation, { DeleteTodo } from "./DeleteTodo";
 
-//import dustbin from "../assets/dustbin.png";
+export default function TodoItems({ onDelete, todo, onUpdate }) {
+  const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const storageKey = `todo-${todo.id}-checked`;
+  const [isStrike, setIsStrike] = useState(() => {
+    return JSON.parse(localStorage.getItem(storageKey)) || false;
+  });
 
-export default function TodoItems({ text }) {
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(isStrike));
+  }, [isStrike]);
+
+  const handelCheckboxChange = (checked) => {
+    setIsStrike(checked);
+  };
   return (
     <div>
-      <p>
-        <Checkbox className="checkbox" /> {text} <Pencil className="edit" />{" "}
-        <Trash2 />
-      </p>
+      <div className="flex-row">
+        <Checkbox todoId={todo.id} onCheck={handelCheckboxChange} />
+        <div style={{ textDecoration: isStrike ? "line-through" : "none" }}>
+          {todo.text}
+        </div>
+        <ClickEdit onClickEdit={() => setIsEditing(true)} />
+        <EditTodo
+          isOpen={isEditing}
+          onClose={() => setIsEditing(false)}
+          todo={todo}
+          onUpdate={onUpdate}
+        />
+        <DeleteTodo onDelete={() => setShowConfirmationDialog(true)} />
+        <DeleteConfirmation
+          isOpen={showConfirmationDialog}
+          onConfirm={() => {
+            onDelete(todo.id);
+            setShowConfirmationDialog(false);
+          }}
+          onCancel={() => setShowConfirmationDialog(false)}
+        />
+      </div>
     </div>
   );
 }
