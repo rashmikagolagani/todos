9c8797911bce4ab104d874a4ea0851600acbc9c1
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-03-13 12:51:16 +0530

    useEffect is used in the checkbox in order to render the checkbox after the component is rendered

--- src/Tickbox.jsx
+++ src/Tickbox.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
-export default function Checkbox() {
-  const [checkbox, setCheckbox] = useState("");
+import React, { useState, useEffect } from "react";
+
+export default function Checkbox({ onCheck, todoId }) {
+  const storageKey = `todo-${todoId}-checked`;
+
+  const [checkbox, setCheckbox] = useState(() => {
+    return JSON.parse(localStorage.getItem(storageKey)) || false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(checkbox));
+  }, [checkbox]);
+
+  function handleChange(e) {
+    const newCheckedState = e.target.checked;
+    setCheckbox(newCheckedState);
+    onCheck(newCheckedState);
+  }
+
   return (
     <input
+      className="checkbox"
       type="checkbox"
       checked={checkbox}
-      onChange={(e) => setCheckbox(e.target.checked)}
-      required
+      onChange={handleChange}
     />
   );
 }
